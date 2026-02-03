e3701ddc9b379a81fa1097dac60ad7abe3e2ed65
baekhwa <light797@naver.com>
2026-02-03 18:33:46 +0900

    [02/03] 시멘틱 마크업 변경

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,6 @@ import homeArrow from '../../assets/images/home/home-arrow.svg';
 import homeIconPlus from '../../assets/images/home/home-icon-plus.svg';
 import homePointImg from '../../assets/images/home/home-point-img.png';
 
-
 /**
  * 롤링 페이퍼 홈 페이지 컴포넌트
  * 
@@ -20,73 +19,71 @@ import homePointImg from '../../assets/images/home/home-point-img.png';
 
 function Home() {
 
-  return <>
-
-
-    <div className={styles.container}>
+  return (
+    <main className={styles.container}>
       <section className={styles.contents}>
 
-        <div className={`${styles.point} ${styles.point01Box}`}>
-          <div className={styles.pointText}>
-            <p className={styles.pointBadge}><span >Point. 01</span></p>
-            <h2>누구나 손쉽게, 온라인<br /> 롤링 페이퍼를 만들 수 있어요</h2>
+        <section className={`${styles.point} ${styles.point01Box}`} aria-labelledby="point01-heading">
+          <header className={styles.pointText}>
+            <p className={styles.pointBadge}><span>Point. 01</span></p>
+            <h2 id="point01-heading">누구나 손쉽게, 온라인<br /> 롤링 페이퍼를 만들 수 있어요</h2>
             <p>로그인 없이 자유롭게 만들어요.</p>
-          </div>
+          </header>
 
           <div className={styles.fromCades}>
-            <div className={styles.fromCade}>
-              <div className={styles.fromCadeLine}>
+            <article className={styles.fromCade}>
+              <figure className={styles.fromCadeLine}>
                 <p className={styles.fromCadeImg}><img src={homePhoto} alt="강미나" /></p>
-                <p className={styles.fromCadeName}>
+                <figcaption className={styles.fromCadeName}>
                   <span>From.&nbsp;</span>
                   <span>강미나</span>
                   <span className={styles.fromCadeBadge}>친구</span>
-                </p>
-              </div>
+                </figcaption>
+              </figure>
               <p className={styles.fromCadeText}>코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!</p>
-              <p className={styles.fromCadeDate}><span>2026.01.26</span></p>
-            </div>
+              <time className={styles.fromCadeDate} dateTime="2026-01-26"><span>2026.01.26</span></time>
+            </article>
 
-            <div className={styles.fromCade}>
-              <div className={styles.fromCadeLine}>
-                <p className={styles.fromCadeImg}><img src={homePhoto} alt="강미나" /></p>
-                <p className={styles.fromCadeName}>
+            <article className={styles.fromCade}>
+              <figure className={styles.fromCadeLine}>
+                <p className={styles.fromCadeImg}><img src={homePhoto} alt="박대영" /></p>
+                <figcaption className={styles.fromCadeName}>
                   <span>From.&nbsp;</span>
                   <span>박대영</span>
                   <span className={`${styles.fromCadeBadge} ${styles.type}`}>친구</span>
-                </p>
-              </div>
+                </figcaption>
+              </figure>
               <p className={styles.fromCadeText}>일교차가 큰 시기입니다. 새벽에는 겨울, 한낮에는 여름, 아침저녁으로는 가을을 느껴보는 것도 좋을 것 같아요. 일교차가 큰 시기입니다. 새벽에는 겨울, 한낮에는 여름, 아침저녁으로는 가을을 느껴보는 것도 좋을 것 같아요.</p>
-              <p className={styles.fromCadeDate}><span>2026.01.26</span></p>
-            </div>
+              <time className={styles.fromCadeDate} dateTime="2026-01-26"><span>2026.01.26</span></time>
+            </article>
 
-            <div className={`${styles.fromCade} ${styles.fromCadeIcon}`}>
+            <div className={`${styles.fromCade} ${styles.fromCadeIcon}`} aria-hidden="true">
               <p className={styles.fromCadeImg01}><img src={homeIconPlus} alt="" /></p>
               <p className={styles.fromCadeImg02}><img src={homeArrow} alt="" /></p>
             </div>
           </div>
-        </div>
+        </section>
 
-        <div className={`${styles.point} ${styles.point02Box}`}>
-          <div className={styles.pointImg}>
-            <img src={homePointImg} alt="" />
-          </div>
-          <div className={styles.pointText}>
-            <p className={styles.pointBadge}><span >Point. 02</span></p>
-            <h2>서로에게 이모지로 감정을 <br /> 표현해보세요</h2>
+        <section className={`${styles.point} ${styles.point02Box}`} aria-labelledby="point02-heading">
+          <figure className={styles.pointImg}>
+            <img src={homePointImg} alt="이모지로 감정 표현하기" />
+          </figure>
+          <header className={styles.pointText}>
+            <p className={styles.pointBadge}><span>Point. 02</span></p>
+            <h2 id="point02-heading">서로에게 이모지로 감정을 <br /> 표현해보세요</h2>
             <p>롤링 페이퍼에 이모지를 추가할 수 있어요.</p>
-          </div>
-        </div>
-        <div className={styles.btnView}>
+          </header>
+        </section>
+
+        <nav className={styles.btnView}>
           <Link to="/list" className={styles.linkButton}>
             구경해 보기
           </Link>
-        </div>
+        </nav>
 
       </section>
-    </div >
-
-  </>;
+    </main>
+  );
 }
 
 export default Home;
