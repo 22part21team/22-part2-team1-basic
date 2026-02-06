1f7f413e4b4c85182ba70ff71872e2d54dd624f8
anarose314 <dpgp706@gmail.com>
2026-02-06 17:45:04 +0900

    Feat: 공통 헤더 CSS 수정 및 /post 와 /messages 에 '이전으로' 버튼 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, matchPath, useLocation, useNavigate } from 'react-router-dom';
 import logoMain from '/src/assets/images/common/logo-main.svg';
 import styles from './Header.module.css';
 
@@ -12,8 +12,11 @@ const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  // / 또는 /list 페이지에서만 버튼 표시
+  // / 또는 /list 페이지에서만 "롤링페이퍼 만들기" 버튼 표시
   const showCreateButton = location.pathname === '/' || location.pathname === '/list';
+  // /post 또는 /post/{id}/message 페이지에서만 "이전으로" 버튼 표시
+  const showPrevButton =
+    location.pathname === '/post' || !!matchPath({ path: '/post/:id/message' }, location.pathname);
   // /post 하위 페이지에만 moblieHide 추가
   const isLocation = location.pathname.startsWith('/post');
   const headerClass = isLocation ? `${styles.header} ${styles.mobileHide}` : `${styles.header}`;
@@ -25,6 +28,18 @@ const Header = () => {
     navigate('/post');
   };
 
+  /**
+   * 이전으로 버튼 클릭 핸들러
+   */
+  const handlePrevClick = () => {
+    // 외부에서 바로 온 경우, 홈페이지로 이동
+    if (window.history.length <= 2) {
+      navigate('/');
+    } else {
+      navigate(-1);
+    }
+  };
+
   return (
     <header className={headerClass}>
       <div className={styles.container}>
@@ -33,8 +48,29 @@ const Header = () => {
         </Link>
 
         {showCreateButton && (
-          <button className={styles.createButton} onClick={handleCreateClick}>
+          <button className={styles.headerButton} onClick={handleCreateClick}>
             롤링 페이퍼 만들기
+          </button>
+        )}
+
+        {showPrevButton && (
+          <button className={styles.headerButton} onClick={handlePrevClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+              width="1em"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+            <span className={styles.headerSpan}>이전으로</span>
           </button>
         )}
       </div>
