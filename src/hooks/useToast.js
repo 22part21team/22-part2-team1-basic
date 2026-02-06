83786958e9b243638163f87d61565ab8cf596807
anarose314 <dpgp706@gmail.com>
2026-02-06 19:41:46 +0900

    Feat: [/post/{id}] 이모지 추가 후 토스트 알림 추가

--- src/hooks/useToast.js
+++ src/hooks/useToast.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useRef } from 'react';
 
 /**
  * 토스트 메시지 관리 커스텀 훅
@@ -6,12 +6,26 @@ import { useState, useCallback } from 'react';
  */
 export const useToast = () => {
   const [toast, setToast] = useState({ show: false, message: '' });
+  const timerRef = useRef(null);
 
   const showToast = useCallback((message) => {
-    setToast({ show: true, message });
+    // 이미 실행 중인 토스트가 있는 경우, 진행 중이던 타이머 취소
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    // 이미 실행 중인 토스트가 있는 경우를 방지하기 위한 상태 초기화
+    setToast({ show: false, message });
+
+    // 토스트 동작 실행
     setTimeout(() => {
-      setToast({ show: false, message: '' });
-    }, 3000);
+      setToast({ show: true, message });
+
+      // 3초 뒤에 꺼지도록 설정
+      timerRef.current = setTimeout(() => {
+        setToast({ show: false, message: '' });
+        timerRef.current = null;
+      }, 3000);
+    }, 10);
   }, []);
 
   return { toast, showToast };
